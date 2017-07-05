460c2a4e77b0cef27de7d549f6ca0b95d5b8548a
algo1970 <hida1970@gmail.com>
2017-07-05 11:59:43 +0900

    change data files

--- sample/server.R
+++ sample/server.R
@@ -3,46 +3,24 @@
 library(shiny)
 library(shinythemes)
 library(dplyr)
-library(ggplot2)
-library(plotly)
-library(visNetwork)
-library(DiagrammeR)
-library(leaflet)
 
 # server----
 shinyServer(
-function(input, output) { 
-output$ggplot <- renderPlot({
-ggplot(iris,aes(Sepal.Width,Sepal.Length))+geom_point()
-})
-output$plotly <- renderPlotly({
-plot_ly(z=volcano,type="heatmap")
-})
-output$imageoutput <- renderImage({
-      filename <- normalizePath("../dog300.png")  
-      list(src=filename,width = 300)
-    }, deleteFile = FALSE)
-output$mapLeaflet <- renderLeaflet({
-leaflet() %>% 
-addProviderTiles(providers$Stamen.TonerLite,options = providerTileOptions(noWrap = TRUE))} %>% 
-addMiniMap(tiles = providers$Esri.WorldStreetMap,toggleDisplay = TRUE))
-output$clickImage <- renderImage({
-        list(src = "../ebimiso2.jpg",width=400)
-    }, deleteFile = FALSE)
-    output$infoClickXY <- renderText({
-      paste0("x=",input$image_click$x,"y=",input$image_click$y)
+  function(input, output, session) { 
+    output$fileContents <- renderTable({
+      inFile <- input$file_input1     
+      if (is.null(inFile))
+        return(NULL)
+      read.csv(inFile$datapath, header = input$header)
     })
-output$diagram <- renderGrViz({
-nodes <- create_node_df(3)
-edges <- create_edge_df(from = 1:3,to = c(2,3,1))
-create_graph(nodes,edges) %>% render_graph()
-})
-output$visnetwork <- renderVisNetwork({
-nodes <- data.frame(id = 1:3,label=1:3)
-edges <- data.frame(from = c(1,2,3), to = c(2,3,1))
-visNetwork(nodes, edges)})
-output$verba_text <- renderText({
-text <- "sample text"
-})
-}
+    comment_save <- eventReactive(input$action_button,{
+      cat("saveButton was pushed"
+      )})
+    output$comment_actionButton <- renderText({
+      comment_save()
+    })
+    output$txt <- renderText({
+      icons <- paste(input$checkbox_group_input,collapse = ", ");paste("You chose", icons)
+    })
+  }
 )
--- sample/ui.R
+++ sample/ui.R
@@ -3,45 +3,35 @@
 library(shiny)
 library(shinythemes)
 library(dplyr)
-library(ggplot2)
-library(plotly)
-library(visNetwork)
-library(DiagrammeR)
-library(leaflet)
 
 # ui----
 shinyUI(
-fluidPage(
-titlePanel("sample"),
-fluidRow(
-column(4,
- textInput("title", label="title",value="") 
-),
-column(4,
- plotOutput("ggplot",height=200) 
-),
-column(4,
- plotlyOutput("plotly") 
-),
-column(4,
- imageOutput("imageoutput") 
-),
-column(4,
- leafletOutput("mapLeaflet") 
-),
-column(4,
- imageOutput("clickImage",click = "image_click"),
-verbatimTextOutput("infoClickXY") 
-),
-column(4,
- grVizOutput("diagram",width ="100%",height="300px") 
-),
-column(4,
- visNetworkOutput("visnetwork") 
-),
-column(4,
- verbatimTextOutput("verba_text") 
-)
-)
-)
+  fluidPage(
+    titlePanel("sample"),
+    fluidRow(
+      column(4,
+             textInput("title", label="title",value="") 
+      ),
+      column(4,
+             dateInput("date_input", "Date:") 
+      ),
+      column(4,
+             dateRangeInput("daterange_input", "Date range:",start = Sys.Date()-10,end = Sys.Date()+10) 
+      ),
+      column(4,
+             fileInput("file_input",label = "Input File:"),
+             tableOutput("fileContents") 
+      ),
+      column(4,
+             actionButton("action_button","save data"),
+             verbatimTextOutput("comment_actionButton") 
+      ),
+      column(4,
+             checkboxGroupInput("checkbox_group_input", "Choose icons:",
+                                choiceNames=list(icon("calendar"),icon("bed")),
+                                choiceValues=list("calendar","bed")),
+             textOutput("txt") 
+      )
+    )
+  )
 )
